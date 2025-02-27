bea59bdeba5e364da0be5a0fc172ec8b054a9f14
unknown <you@example.com>
2025-02-27 10:25:06 +0700

    Day 1 2702025 10.22 AM

---
+++ my-e-commerce/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>
+            {/* nếu menu là cái đang click thì thêm thẻ hr vô */}
+          Shop{menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          Men{menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("womens")}>
+          Women{menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          Kids{menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
