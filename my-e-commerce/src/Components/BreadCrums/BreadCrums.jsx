7a2866deec34c23bfc76582928785de939930d5e
“buiquocdat30” <buiquocdat30@gmail.com>
2025-03-07 20:14:06 +0700

    Day 5 07032025 20h15 PM

---
+++ my-e-commerce/src/Components/BreadCrums/BreadCrums.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./BreadCrums.css";
+import arrow_icon from "../Assets/breadcrum_arrow.png";
+const BreadCrums = (props) => {
+  const { product } = props;
+
+  if (!product.category) {
+    console.log("lỗi ko có data");
+  } else {
+    return (
+      <div className="breadcrum">
+        HOME <img src={arrow_icon} alt="" /> SHOP{" "}
+        <img src={arrow_icon} alt="" /> {product.category}{" "}
+        <img src={arrow_icon} alt="" /> {product.name}
+      </div>
+    );
+  }
+};
+
+export default BreadCrums;
--- my-e-commerce/src/Pages/Product.jsx
+++ my-e-commerce/src/Pages/Product.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router";
+import BreadCrums from "../Components/BreadCrums/BreadCrums";
+const Product = () => {
+  const { all_product } = useContext(ShopContext);
 
- const Product = () => {
+  const { productId } = useParams();
+
+  const product = all_product.find((e) => e.id === Number(productId));
+
   return (
-    <div>Product</div>
-  )
-}
+    <div>
+      <BreadCrums product={product} />
+      {console.log("ben pro ", product)}
+    </div>
+  );
+};
 
 export default Product;
